a9500b69711a2f6767d82293227f1ec842b0b6fa
juan-em <9jescobarm6@gmail.com>
2023-08-30 14:00:17 -0500

    commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, 
+         View, 
+         StyleSheet, 
+         Image, 
+         TouchableOpacity, 
+         Alert,
+         TextInput } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hola Mundo!</Text>
+      <Image
+        source={{uri:'https://picsum.photos/200/300'}}
+        style={styles.image}
+      />
+      <TouchableOpacity
+        onPress={()=> Alert.alert('Holaaa')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Presionar aquí</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe algo aquí"
+        placeholderTextColor="#FFFFFF"
+      />
     </View>
-  );
-}
+    
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#292929'
   },
-});
+  title:{fontSize: 30, color:'#fff'},
+  image:{height:200, width:300, borderRadius:90},
+  button:{
+    backgroundColor:'blue',
+    padding:7,
+    margiTop: 10
+  },
+  buttonText: {
+    color: "#fff", fontSize: 20
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 10,
+    width: 300,
+  }
+})
+
+
+export default App;
